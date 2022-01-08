5c400e4b50bb25a7a31417e772922d1ab26ebacf
Aron Radics <aron.radics.jozsef@gmail.com>
2022-01-08 14:36:45 +0100

    Increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("Readme.rst", "r") as fh:
 
 setuptools.setup(
     name="ncoreparser",
-    version="1.7.1",
+    version="1.7.2",
     author="Aron Radics",
     author_email="radics.aron.jozsef@gmail.com",
     description="Package to download from ncore.pro",
